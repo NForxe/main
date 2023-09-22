be0922461a474c359d9478701323da62f424ad80
Nikita Forxe <96699242+NForxe@users.noreply.github.com>
2023-09-22 22:28:02 +0300

    Add files via upload

--- script/js.js
+++ script/js.js
@@ -1,3 +1,11 @@
+function fa(n) {
+  var res = 1;
+  for (var i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
+}
+
 function pr1() {
   var x = Number(document.forms["frm"].elements["x"].value),
     n = Number(document.forms["frm"].elements["n"].value),
@@ -10,10 +18,17 @@ function pr1() {
   document.writeln(sum);
 }
 
-function fa(n) {
-  var res = 1;
-  for (var i = 1; i <= n; i++) {
-    res *= i;
+// Дана последовательность целых чисел а1, а2, …, аn Выяснить какое число встречается раньше – положительное или отрицательное
+
+function pr2() {
+  var arr = [2, -5, 7, -3, 0, 9];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      document.writeln(`Первое число положительно ${arr[i]} <br>`);
+    } else if (arr[i] < 0) {
+      document.writeln(`Первое число отрицательное ${arr[i]}<br>`);
+    } else {
+      document.writeln("Нет ни положительных, ни отрицательных чисел <br>");
+    }
   }
-  return res;
 }
