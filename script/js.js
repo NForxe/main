95bf2d3b16988d003b6d74f1644ad0cfac159a9b
Nikita Forxe <96699242+NForxe@users.noreply.github.com>
2023-09-22 22:09:40 +0300

    js.js

--- script/js.js
+++ script/js.js
@@ -1,18 +1,19 @@
 function pr1() {
   var x = Number(document.forms["frm"].elements["x"].value),
-      n = Number(document.forms["frm"].elements["n"].value), 
-      sum = 0, sum1 = 1;
-  for(var i = 1; i <= n; i++){
-    var res = (((-1) ** i) * (x ** i)) / (fa(i));
+    n = Number(document.forms["frm"].elements["n"].value),
+    sum = 0,
+    sum1 = 1;
+  for (var i = 1; i <= n; i++) {
+    var res = ((-1) ** i * x ** i) / fa(i);
     sum = sum + res;
   }
   document.writeln(sum);
 }
 
 function fa(n) {
-var res = 1;
-for (var i = 1; i <= n; i++){
- res *= i;
-}
-return res;
+  var res = 1;
+  for (var i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 }
